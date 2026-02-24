0c52679fbd66a7a8aeaa1b54ec7a5576e36e2c74
harshitha reddy <harshitha reddy@harshitha>
2026-02-24 12:50:12 +0530

    kj

--- ComcastCRMGUIFramework/src/main/java/com/contast/crm/objectrepositoryutility/Products.java
+++ ComcastCRMGUIFramework/src/main/java/com/contast/crm/objectrepositoryutility/Products.java
@@ -1,5 +1,10 @@
 package com.contast.crm.objectrepositoryutility;
 
-public class Products {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class Products 
+{
+	@FindBy(xpath="//input[@alt='Create Product...']")
+	private WebElement createProductImgBtn;
 }
